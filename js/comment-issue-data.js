079f7a867f56a1ec161c001047c984595f9dc457
cjhcjh6 <xxscac@outlook.com>
2024-05-05 00:15:04 +0800

    添加余梦再现目前进展09

--- js/comment-issue-data.js
+++ js/comment-issue-data.js
@@ -1,4 +1,3 @@
-// 评论issues仓库 by.removeif https://removeif.github.io/
 var repoIssuesUrl = "https://api.github.com/repos/userName/userRepo/issues";
 // 对应仓库 clientId、clientSecret 关于这两个参数的安全问题，查看 https://removeif.github.io/2019/09/19/博客源码分享.html#1-热门推荐，最新评论：
 var clientId;// = "46a9f3481b46ea0129d8";
@@ -334,7 +333,7 @@ function loadIssueData(appId, appKey, userName, userRepo, isValine) {
             }
         }
         // console.clear();
-        console.log("~~~~欢迎光临！记得有时间多来看看哦，https://removeif.github.io/ ~~~~")
+        console.log("~~~~欢迎光临！记得有时间多来看看哦，https://cjhcjh6.top/ ~~~~")
     }
         ,
         500
